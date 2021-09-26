7108f16116d7d97c03c6ae269ec337d31b990161
JalalMadhaji <jalal.almadhaji90@gmail.com>
2021-09-27 02:28:09 +0300

    Update MainActivity.kt

--- app/src/main/java/com/plcoding/cryptocurrencyappyt/presentation/MainActivity.kt
+++ app/src/main/java/com/plcoding/cryptocurrencyappyt/presentation/MainActivity.kt
@@ -5,15 +5,31 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.plcoding.cryptocurrencyappyt.presentation.coinDetail.components.CoinDetailScreen
+import com.plcoding.cryptocurrencyappyt.presentation.coinList.components.CoinListScreen
 import com.plcoding.cryptocurrencyappyt.presentation.ui.theme.CryptocurrencyAppYTTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             CryptocurrencyAppYTTheme {
                 Surface(color = MaterialTheme.colors.background) {
-
+                    val navController = rememberNavController()
+                    NavHost(navController = navController,
+                    startDestination = Screen.CoinListScreen.route){
+                        composable(route = Screen.CoinListScreen.route){
+                            CoinListScreen(navController = navController)
+                        }
+                        composable(route = Screen.CoinDetailScreen.route + "/{coin-id}"){
+                            CoinDetailScreen()
+                        }
+                    }
                 }
             }
         }
