b53d090b4be2c82fc4d37107c3a4c9d87a4107ea
JalalMadhaji <jalal.almadhaji90@gmail.com>
2021-09-27 02:27:47 +0300

    Update GetCoinUseCase.kt

--- app/src/main/java/com/plcoding/cryptocurrencyappyt/domain/useCase/getCoin/GetCoinUseCase.kt
+++ app/src/main/java/com/plcoding/cryptocurrencyappyt/domain/useCase/getCoin/GetCoinUseCase.kt
@@ -13,13 +13,13 @@ import javax.inject.Inject
 class GetCoinUseCase @Inject constructor(private val repository: CoinRepository) {
     operator fun invoke(coinId : String) : Flow<Resource<CoinDetail>> = flow{
         try {
-            emit(Resource.Loading())
+            emit(Resource.Loading<CoinDetail>())
             val coinDetail = repository.getCoinById(coinId).toCoinDetail()
-            emit(Resource.Success(coinDetail))
+            emit(Resource.Success<CoinDetail>(coinDetail))
         }catch (e : HttpException){
-            emit(Resource.Error(e.localizedMessage ?: "An unexpected HTTP error occurred"))
+            emit(Resource.Error<CoinDetail>(e.localizedMessage ?: "An unexpected HTTP error occurred"))
         }catch (e : IOException){
-            emit(Resource.Error(e.localizedMessage ?: "Could not reach server, check your connection"))
+            emit(Resource.Error<CoinDetail>(e.localizedMessage ?: "Could not reach server, check your connection"))
         }
     }
 }
