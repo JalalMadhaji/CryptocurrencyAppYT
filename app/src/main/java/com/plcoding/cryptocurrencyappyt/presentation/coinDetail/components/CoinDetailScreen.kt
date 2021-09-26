f597aaa29f9611da2e3d8323b48b20babc80161e
JalalMadhaji <jalal.almadhaji90@gmail.com>
2021-09-27 02:27:59 +0300

    Update CoinDetailScreen.kt

--- app/src/main/java/com/plcoding/cryptocurrencyappyt/presentation/coinDetail/components/CoinDetailScreen.kt
+++ app/src/main/java/com/plcoding/cryptocurrencyappyt/presentation/coinDetail/components/CoinDetailScreen.kt
@@ -2,7 +2,9 @@ package com.plcoding.cryptocurrencyappyt.presentation.coinDetail.components
 
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.CircularProgressIndicator
+import androidx.compose.material.Divider
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
@@ -11,19 +13,19 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
+import com.google.accompanist.flowlayout.FlowRow
 import com.plcoding.cryptocurrencyappyt.presentation.coinDetail.CoinDetailViewModel
 
 @Composable
 fun CoinDetailScreen(
     viewModel: CoinDetailViewModel = hiltViewModel()
-){
+) {
     val state = viewModel.state.value
     Box(modifier = Modifier.fillMaxSize()) {
         state.coinDetail?.let { coin ->
-            LazyColumn(modifier = Modifier.fillMaxSize()){
+            LazyColumn(modifier = Modifier.fillMaxSize().padding(16.dp)) {
                 item {
                     Row(
                         modifier = Modifier.fillMaxWidth(),
@@ -35,20 +37,50 @@ fun CoinDetailScreen(
                             modifier = Modifier.weight(8f)
                         )
                         Text(
-                            text = if(coin.isActive) "active" else "inactive",
-                            color = if(coin.isActive) Color.Green else Color.Red,
+                            text = if (coin.isActive) "active" else "inactive",
+                            color = if (coin.isActive) Color.Green else Color.Red,
                             fontStyle = FontStyle.Italic,
                             textAlign = TextAlign.End,
-                            modifier = Modifier.align(Alignment.CenterVertically)
+                            modifier = Modifier
+                                .align(Alignment.CenterVertically)
                                 .weight(2f)
 
                         )
                     }
+                    Spacer(modifier = Modifier.height(15.dp))
+                    Text(
+                        text = coin.description,
+                        style = MaterialTheme.typography.body2
+                    )
+                    Spacer(modifier = Modifier.height(15.dp))
+                    Text(
+                        text = "Tags",
+                        style = MaterialTheme.typography.h3
+                    )
+                    Spacer(modifier = Modifier.height(15.dp))
+                    FlowRow(
+                        mainAxisSpacing = 10.dp,
+                        crossAxisSpacing = 10.dp,
+                        modifier = Modifier.fillMaxWidth()
+                    ) {
+                        coin.tags.forEach { tag ->
+                            CoinTag(tag = tag)
+                        }
+                    }
+                    Spacer(modifier = Modifier.height(15.dp))
+                    Text(text = "Team Members", style = MaterialTheme.typography.h3)
+                    Spacer(modifier = Modifier.height(15.dp))
+                }
+                items(coin.team){ member ->
+                    TeamListItem(team = member, modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(10.dp))
+                    Divider()
                 }
             }
         }
 
-        if(state.error.isNotBlank()){
+        if (state.error.isNotBlank()) {
             Text(
                 text = state.error,
                 color = MaterialTheme.colors.error,
@@ -60,7 +92,7 @@ fun CoinDetailScreen(
             )
         }
 
-        if(state.isLoading){
+        if (state.isLoading) {
             CircularProgressIndicator(modifier = Modifier.align(Alignment.Center))
         }
     }
